0a9eba88958d9da26d200e2078089b98d63b5884
Greg Bailey <gregbailey617@gmail.com>
2024-10-26 12:26:33 -0400

    styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,7 +11,6 @@ function Footer() {
         <div className="right">
           <p><a href="https://github.com/zmuda44">github</a></p>
           <p><a href="https://www.linkedin.com/in/greg-bailey-a2a13887/">linkedin</a></p>
-          <p><a href="https://stackexchange.com/users/30242602/greg">stack overflow</a></p>
         </div> 
       </div>       
     </footer>
